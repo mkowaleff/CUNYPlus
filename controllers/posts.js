f3ec30aa07dbab43a265ccfb76e2dacfa2842b63
Min Chen <min75208@yahoo.com>
2016-09-30 18:40:55 -0400

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,16 +1,24 @@
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-var server = require('../../app');
-var expect = chai.expect;
+var express = require('express');
+var router = express.Router();
 
-chai.use(chaiHttp);
-
-
-describe('Posts Controller (Version 1)', function() {
-  it('should list ALL posts on /posts/ GET');
-  it('should list a SINGLE post on /posts/:title GET');
-  it('should add a SINGLE post on /posts POST');
-  it('should add a SINGLE reply post on /posts/:title PUT')
-  it('should update a SINGLE post on /posts/:title PUT');
-  it('should delete a SINGLE post on /posts/:title DELETE');
+router.get('/', function (req, res){
+ 	res.send('Groups');
 });
+
+router.post('/createPost', function (req, res){
+ 	res.send('Create a new post');
+});
+
+router.delete('/deletePost', function(req, res){
+  res.send('Delete a post');
+});
+
+router.get('/:title', function(req, res){
+  res.send('List a single post');
+});
+
+router.put('/:title', function(req, res){
+  res.send('Add a single reply post');
+});
+
+module.exports = router; 
