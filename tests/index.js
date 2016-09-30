5ea56cd336f19189f5730f3832ccfdd67f37bff2
Min Chen <min75208@yahoo.com>
2016-09-30 18:18:23 -0400

    Update index.js

--- tests/index.js
+++ tests/index.js
@@ -8,9 +8,9 @@ chai.use(chaiHttp);
 
 describe('index controller', function() {
   it('should display home page on / GET');
-  it('should change to activies page /activities USE' );
-  it('should change to major page /major USE');
-  it('should change to groups page /group USE');
-  it('should change to login page /login USE');
-  it('should change to signup page /signup USE');
+  it('should change to activies page on /activities USE' );
+  it('should change to major page on /major USE');
+  it('should change to groups page on /group USE');
+  it('should change to login page on /login USE');
+  it('should change to signup page on /signup USE');
 });
