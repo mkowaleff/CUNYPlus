a9131ae90358c50991ef8774e862cac354752c6e
Martin Kowaleff <martin.kowaleff@lc.cuny.edu>
2016-12-09 19:09:07 -0500

    Add files via upload

--- models/post.js
+++ models/post.js
@@ -1,61 +1,31 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
-// User Schema
-var UserSchema = mongoose.Schema({
-    username: {
+// Post Schema
+var postSchema = mongoose.Schema({
+    title: {
       type: String
     },
-    password: {
+    link: {
       type:String
     },
-    password2: {
-      type:String
-    },
-    email:{
-      type:String,
-      index: true
-    },
-    gender: {
-      type:String
-    },
-    major: {
-      type:String
-    },
-    minor: {
-      type:String
-    },
-    school: {
-      type:String
-    },
-    intro: {
+    description: {
       type:String
     }
 });
 
-var User = module.exports = mongoose.model('User', UserSchema);
+var Post = module.exports = mongoose.model('Post', PostSchema);
 
-module.exports.createUser = function(newUser, callback){
-	bcrypt.genSalt(10, function(err, salt) {
-	    bcrypt.hash(newUser.password, salt, function(err, hash) {
-	        newUser.password = hash;
-	        newUser.save(callback);
-	    });
-	});
+module.exports.createPost = function(newPost, callback){
+	        newPost.save(callback);
 }
 
-module.exports.getUserByEmail = function(email, callback){
-	var query = {email: email};
+module.exports.getPostByTitle = function(title, callback){
+	var query = {title: title};
 	User.findOne(query, callback);
 }
 
-module.exports.getUserById = function(id, callback){
+module.exports.getPostById = function(id, callback){
 	User.findById(id, callback);
 }
-
-module.exports.comparePassword = function(candidatePassword, hash, callback){
-	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-    	if(err) throw err;
-    	callback(null, isMatch);
-	});
 }
