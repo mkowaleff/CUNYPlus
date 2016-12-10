bb754cf222d2997cf3feb1da3ecfb660a75afcd1
Min Chen <min75208@gmail.com>
2016-12-09 19:00:09 -0500

    commit

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var Post = require('../models/post');
+
 router.get('/', function (req, res){
  	res.render('posts');
 });
 
 router.post('/createPost', function (req, res){
- 	res.send('Create a new post');
+
+  var newPost = new Post({
+    title: req.body.title,
+    link: req.body.link,
+    description: req.body.description
+  });
+
+  Post.createPost(newPost, function(err, post){
+    if(err) throw err;
+    console.log(post);
+  });
+
+ 	res.redirect('/discussions');
 });
 
 router.delete('/deletePost', function(req, res){
@@ -21,4 +35,4 @@ router.put('/:title', function(req, res){
   res.send('Add a single reply post');
 });
 
-module.exports = router; 
+module.exports = router;
---
+++ models/post.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// User Schema
+var UserSchema = mongoose.Schema({
+    username: {
+      type: String
+    },
+    password: {
+      type:String
+    },
+    password2: {
+      type:String
+    },
+    email:{
+      type:String,
+      index: true
+    },
+    gender: {
+      type:String
+    },
+    major: {
+      type:String
+    },
+    minor: {
+      type:String
+    },
+    school: {
+      type:String
+    },
+    intro: {
+      type:String
+    }
+});
+
+var User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.createUser = function(newUser, callback){
+	bcrypt.genSalt(10, function(err, salt) {
+	    bcrypt.hash(newUser.password, salt, function(err, hash) {
+	        newUser.password = hash;
+	        newUser.save(callback);
+	    });
+	});
+}
+
+module.exports.getUserByEmail = function(email, callback){
+	var query = {email: email};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
